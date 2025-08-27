c34eaaaeca4329c1451c3c77e98b4670fe23b891
Jxtthere <jassajattt01@gmail.com>
2025-08-27 22:13:33 +0530

    Update war.js

--- Rudra/commands/war.js
+++ Rudra/commands/war.js
@@ -6,7 +6,7 @@ module.exports.config = {
     description: "War in group boxchat",
     commandCategory: "group",
     usages: "bold war",
-    cooldowns: 10,
+    cooldowns: 17,
     dependencies: {
         "fs-extra": "",
         "axios": ""
@@ -21,31 +21,31 @@ module.exports.run = async function({ api, args, Users, event}) {
         arraytag.push({id: mention});
     var a = function (a) { api.sendMessage(a, event.threadID); }
 a("Listen to your father, kids !");
-setTimeout(() => {a({body: "ENTER BEFIKRA VELLY JASSA INXIDE HERE BABY" })}, 3000);
+setTimeout(() => {a({body: "ENTER BEFIKRA VELLY JASSA INXIDE HERE BABY🫨❤️👍" })}, 3000);
 setTimeout(() => {a({body: "TERII MAAA DAA FUDDA TU JASSE NU GAAL KADDE 🫨❤️👍"})}, 5000);
-setTimeout(() => {a({body: "TRRII BHAIN DA SHOLA YU JASSA JASSA KRE FUDI DYA" })}, 7000);
-setTimeout(() => {a({body: "TERII YRR MAAA DAA FUDDA CHEER DENA AJJ MAI JE TU JASSE NU ABUXE KITA TA" })}, 9000);
-setTimeout(() => {a({body: "TERII SUTTI PYII MAA UPR JASSA CARH JAVE YAARR OKIEEEE" })}, 12000);
-setTimeout(() => {a({body: "ENTER TUHADI MAAA DAA FUDDA MAARN WALA JASSA KANJAR" })}, 15000);
+setTimeout(() => {a({body: "TRRII BHAIN DA SHOLA YU JASSA JASSA KRE FUDI DYA❤️⚠️" })}, 7000);
+setTimeout(() => {a({body: "TERII YRR MAAA DAA FUDDA CHEER DENA AJJ MAI JE TU JASSE NU ABUXE KITA TA😂❤️👍" })}, 9000);
+setTimeout(() => {a({body: "TERII SUTTI PYII MAA UPR JASSA CARH JAVE YAARR OKIEEEE🙂🧡" })}, 12000);
+setTimeout(() => {a({body: "ENTER TUHADI MAAA DAA FUDDA MAARN WALA JASSA KANJAR☺️👍" })}, 15000);
 setTimeout(() => {a({body: "TUHADIIYAA BHAINN TEE CHADD KE AA GYA JASSAA OYEEE 🙄🩷✋" })}, 17000);
-setTimeout(() => {a({body: "TUHADII MAAA DII FUDI THOKI JASSE NE HAHAHAHAH" })}, 20000);
+setTimeout(() => {a({body: "TUHADII MAAA DII FUDI THOKI JASSE NE HAHAHAHAH🥹🤙" })}, 20000);
 setTimeout(() => {a({body: "JASSE NE TUHADII MAA PAAD DENI AJJ FER ☺️👍" })}, 23000);
-setTimeout(() => {a({body: "TERE GREEEB DII BHAIN NU LOLL MAAR GYA JASSA HAHAHAHAH" })}, 25000);
-setTimeout(() => {a({body: "LUN PATT LO JASSE DA OYEE FUDIXONE DEOO" })}, 28500);
-setTimeout(() => {a({body: "JASSE NE TUHADIYAA MAAVAA PAAD DENIYA AJJ HAHAHAH" })}, 31000);
-setTimeout(() => {a({body: "HUN TERII MAAA CHUD GYII KADD GAAL JASSE NU FUDI DYA?" })}, 36000);
-setTimeout(() => {a({body: "CHLO WARI WARI LUN FDLO AKE JASSE JATT DA OKIEEE" })}, 39000);
-setTimeout(() => {a({body: "TERII KUTTI MAAA TE JASSSE NE CHD JANA AJJ CHPLAA LAAH KE OYEE BURRAAAAAAA" })}, 40000);
-setTimeout(() => {a({body: "TERII MAA DAAAA GHUXA BOL KUCH ??" })}, 65000);
-setTimeout(() => {a({body: "TUHADIYAA BHAINAA NU LOLL THOK GYA JASS JALANDHARIYA" })}, 70000);
-setTimeout(() => {a({body: "TERII NAAANII DAAA SHOLA MAAR GYA JASSA VELLY" })}, 75000);
-setTimeout(() => {a({body: "GASTII DEE BCHE HUN PJDE FIDE APNE JASS JIJE KOLO DARKE HAHAHAH" })}, 80000);
-setTimeout(() => {a({body: "TUHADIYAAA MAAA DAA YAAR JASSAA HI REHNA NAAM YAAD RAKHOE FUDI DYOO" })}, 85000);
-setTimeout(() => {a("I'm tired and don't curse anymore")} , 90000);
-setTimeout(() => {a({body: "Come on boss update the lyric, let's continue the war" })}, 95000);
-setTimeout(() => {a({body: "Thank you for listening to me war" })}, 100000);
-setTimeout(() => {a({body: "Goodbye and see you in the next program" })}, 105000);
-setTimeout(() => {a({body: "Good bye 🥺"})} , 115000);
+setTimeout(() => {a({body: "TERE GREEEB DII BHAIN NU LOLL MAAR GYA JASSA HAHAHAHAH🫨❤️✋" })}, 25000);
+setTimeout(() => {a({body: "LUN PATT LO JASSE DA OYEE FUDIXONE DEOO✅🧞" })}, 28500);
+setTimeout(() => {a({body: "JASSE NE TUHADIYAA MAAVAA PAAD DENIYA AJJ HAHAHAH🥰👍" })}, 31000);
+setTimeout(() => {a({body: "HUN TERII MAAA CHUD GYII KADD GAAL JASSE NU FUDI DYA?😂😂😂" })}, 36000);
+setTimeout(() => {a({body: "CHLO WARI WARI LUN FDLO AKE JASSE JATT DA OKIEEE🧡😒" })}, 39000);
+setTimeout(() => {a({body: "TERII KUTTI MAAA TE JASSSE NE CHD JANA AJJ CHPLAA LAAH KE OYEE BURRAAAAAAA😝😝" })}, 40000);
+setTimeout(() => {a({body: "TERII MAA DAAAA GHUXA BOL KUCH ??🤤❤️🤙" })}, 65000);
+setTimeout(() => {a({body: "TUHADIYAA BHAINAA NU LOLL THOK GYA JASS JALANDHARIYA😝✋" })}, 70000);
+setTimeout(() => {a({body: "TERII NAAANII DAAA SHOLA MAAR GYA JASSA VELLY😂👍" })}, 75000);
+setTimeout(() => {a({body: "GASTII DEE BCHE HUN PJDE FIDE APNE JASS JIJE KOLO DARKE HAHAHAH🙂⚠️" })}, 80000);
+setTimeout(() => {a({body: "TUHADIYAAA MAAA DAA YAAR JASSAA HI REHNA NAAM YAAD RAKHOE FUDI DYOO😘😘😘" })}, 85000);
+setTimeout(() => {a({body: "TWADII BHAIN DII SHOLI CH THOKAA LUNN OYEE 🙂🧡✋")} , 90000);
+setTimeout(() => {a({body: "NONXTOP LAFFD WJNE AJJ TERI GHODI MAA DE FRR 😆❤️✋" })}, 95000);
+setTimeout(() => {a({body: "TUHADIYAA MAAVA DAA GHUXA MAARUGA JASSA NONSTOP KRO BRABRI 😊🩷🤙" })}, 100000);
+setTimeout(() => {a({body: "MOOT NIKL GYA JAAN HUN TERII BUDI DA JASSE NU DEKH KE DX 🩷😆" })}, 105000);
+setTimeout(() => {a({body: "RIP HOGI DOXT TERII MAAA BYEE  🥺"})} , 115000);
 
 
 
